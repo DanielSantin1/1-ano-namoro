6e478b4a60f854f50ea351a584bcedd955a959cd
Daniel Santin <danielsantin126264@gmail.com>
2025-11-28 09:17:22 -0300

    Adding firebase host

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
 
-const inicio = new Date("2025-12-06"); // ALTERA PARA DATA REAL
+const inicio = new Date("2024-12-06"); // ALTERA PARA DATA REAL
 setInterval(() => {
   const agora = new Date();
   const diff = agora - inicio;
 
   const dias = Math.floor(diff / (1000 * 60 * 60 * 24));
-  document.getElementById("tempo").innerText = 
+  document.getElementById("tempo").innerText =
     dias + " dias vivendo o melhor da vida com você ❤️";
 }, 1000);
 
